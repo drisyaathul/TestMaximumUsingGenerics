f56e1c0f63b2527bf5070818af437cec52ce5560
drisyaathul <drisyak809@gmail.com>
2023-04-24 17:15:21 +0800

    Refactored: Maximum Of three to One Generic Method

--- src/com/bridgelabz/TestMaximum.java
+++ src/com/bridgelabz/TestMaximum.java
@@ -2,76 +2,27 @@ package com.bridgelabz;
 
 public class TestMaximum {
     /*
-        Maximum of Three Variables
+        Ensure the Generic Type extends Comparable
      */
-    public static void maxOfThreeInteger(Integer x, Integer y, Integer z) {
+    public static <T extends Comparable<T>> T maxOfThreeVariables(T x, T y, T z) {
 /*
-    find the maximum of three Integer
-    CompareTo method to test the maximum number
+    CompareTo method to test the maximum of three variables
  */
-        Integer maximum = x;
+        T maximum = x;
         if (y.compareTo(maximum) > 0)
             maximum = y;
-        if (z .compareTo(maximum) > 0)
+        if (z.compareTo(maximum) > 0)
             maximum = z;
-        System.out.println(maximum+ " is the maximum of Three Integers");
+        return maximum;
     }
-    public static void maxOfThreeFloats(Float x, Float y, Float z) {
-/*
-    find the maximum of three Floats
-    CompareTo method to test the maximum number
- */
-        Float maximum = x;
-        if (y.compareTo(maximum) > 0)
-            maximum = y;
-        if (z .compareTo(maximum) > 0)
-            maximum = z;
-        System.out.println(maximum+ " is the maximum of Three Floats");
-    }
-    private static void maxOfThreeStrings(String x, String y, String z) {
- /*
-    find the maximum of three Strings
-    CompareTo method to test the maximum
- */
-        String maximum = x;
-        if (y.compareTo(maximum) > 0)
-            maximum = y;
-        if (z .compareTo(maximum) > 0)
-            maximum = z;
-        System.out.println(maximum+ " is the maximum of Three Strings");
-    }
+
     public static void main(String[] args) {
 
         System.out.println(" *** Maximum Of 3 Variables *** ");
-        /*
-            Find the maximum of three Integers
-         */
-        System.out.println("Maximum of Three Integers :-");
-        //TC1.1: Given MaximumNumber at first position
-        maxOfThreeInteger(60,30, 50);
-        //TC1.2: Given MaximumNumber at second position
-        maxOfThreeInteger(60,80, 50);
-        //TC1.3: Given MaximumNumber at third position
-        maxOfThreeInteger(60,30, 70);
-        /*
-            Find the maximum of three Floats
-         */
-        System.out.println("Maximum of Three Floats :-");
-        //TC1.1: Given MaximumNumber at first position
-        maxOfThreeFloats(5.87f,1.55f,3.28f);
-        //TC1.2: Given MaximumNumber at second position
-        maxOfThreeFloats(3.82f,5.69f,1.2f);
-        //TC1.3: Given MaximumNumber at third position
-        maxOfThreeFloats(2.53f,3.25f,7.6f);
-        /*
-            Find the maximum of three Strings
-         */
-        System.out.println("Maximum of Three Strings :-");
-        //TC1.1: Given Maximum at first position
-        maxOfThreeStrings("Peach","Banana", "Apple");
-        //TC1.2: Given Maximum at second position
-        maxOfThreeStrings("Apple","Peach", "Banana");
-        //TC1.3: Given Maximum at third position
-        maxOfThreeStrings("Banana","Apple", "Peach");
+        //Printing the Call methods
+        System.out.println("Maximum value of Three Integers is "+maxOfThreeVariables(30,50,70));;
+        System.out.println("Maximum value of Three Floats is "+maxOfThreeVariables(2.5f,7.69f,3.24f));
+        System.out.println("Maximum value of Three Strings is "+maxOfThreeVariables("Apple","Peach","Banana"));
+
     }
 }
